08a5ac1da67fc2bfa42a12bc06e54c78602b2c44
Vitor <jvjv10112607@gmail.com>
2024-08-09 19:11:21 -0300

    i reduced the code and made it more efficient

--- src/ex/ex001.rs
+++ src/ex/ex001.rs
@@ -5,55 +5,23 @@ mod func;
 use func::input;
 
 fn main() {
-    // notas:
-    println!("Diga as notas dos três alunos.");
-    
-    let mut nota1 = String::new();
-    let mut nota2 = String::new();
-    let mut nota3 = String::new();
+    let mut g: f64 = 0.0;
+    let mut w: f64 = 0.0;
+    for c in 1..4 {
+        println!("Type the {c}ª grade:");
+        let mut grade = String::new();
+        input(&mut grade);
 
-    // faz a parte do input das notas
-    println!("Nota 1: ");
-    input(&mut nota1);
-    
-    println!("Nota 2: ");
-    input(&mut nota2);
+        println!("Type the weight:");
+        let mut weight = String::new();
+        input(&mut weight);
 
-    println!("Nota 3: ");
-    input(&mut nota3);
+        let conv_g: f64 = grade.trim().parse().expect("Can't convert.");
+        let conv_w: f64 = weight.trim().parse().expect("Can't convert.");
 
-    // converte para números (floats)
-    let conv1: f64 = nota1.trim().parse().expect("Erro ao converter!");
-    let conv2: f64 = nota2.trim().parse().expect("Erro ao converter!");
-    let conv3: f64 = nota3.trim().parse().expect("Erro ao converter!");
-
-    println!("As notas foram: {conv1}, {conv2} e {conv3}");
-
-    // pesos:
-    println!("Diga o peso das notas em ordem respectiva.");
-
-    let mut peso1 = String::new();
-    let mut peso2 = String::new();
-    let mut peso3 = String::new();
-
-    // faz a parte do input do peso das notas
-    println!("Peso 1: ");
-    input(&mut peso1);
-    
-    println!("Peso 2: ");
-    input(&mut peso2);
-
-    println!("Peso 3: ");
-    input(&mut peso3);
-
-    // converte para números (floats)
-    let conv_1: f64 = peso1.trim().parse().expect("Erro ao converter!");
-    let conv_2: f64 = peso2.trim().parse().expect("Erro ao converter!");
-    let conv_3: f64 = peso3.trim().parse().expect("Erro ao converter!");
-
-    println!("Os pesos foram: {conv_1}, {conv_2} e {conv_3}");
-
-    // cálculo da média ponderada e output
-    let media = ((conv1*conv_1) + (conv2*conv_2) + (conv3*conv_3)) / (conv_1 + conv_2 + conv_3);
-    println!("A média ponderada foi: {media:.2}.");
+        g += conv_g * conv_w;
+        w += conv_w;
+    }
+    let avarage: f64 = g / w;
+    println!("The weighted average is {avarage:.2}.");
 }
