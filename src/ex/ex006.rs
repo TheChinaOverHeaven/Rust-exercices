d9fad31dae84f83d64905d189a38e22d23c89110
Vitor <jvjv10112607@gmail.com>
2024-08-21 19:14:12 -0300

    created to solve first-degree functions

---
+++ src/ex/ex006.rs
@@ -0,0 +1,22 @@
+/* Make a 1st grade function calculator */
+mod func;
+use func::input;
+
+fn main() {
+    println!("f(x) = ax + b\n");
+    let mut a = String::new();
+    let mut b = String::new();
+
+    
+    println!("What's 'a' value?");
+    input(&mut a);
+    println!("Whats's 'b' value?");
+    input(&mut b);
+
+    let conv_a:f64 = a.trim().parse().expect("Failed to convert");
+    let conv_b:f64 = b.trim().parse().expect("Failed to convert");
+
+    let x = conv_b * (-1.0) / conv_a;
+
+    println!("The 'x' value is: {x}")
+}
